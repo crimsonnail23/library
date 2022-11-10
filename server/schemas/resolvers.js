f53d211b30a1c3193b1ec6dd6360581adcf76d9c
night <yseddiq@gmail.com>
2022-11-10 16:21:16 -0500

    finished with query in resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,21 @@
 const { User, Book}= require('../models');
 const { AuthenticationError} = require('apollo-server-express');
 const { signToken } = require('../utils/auth')
+
+const resolvers={
+    Query:{
+        me: async (parent, args, context)=>{
+            if(context.user)
+                {
+                    const userData = await User.findOne({ _id: context.user.id })
+                        .select('-__V -password')
+                    return userData;        
+                }
+                throw new AuthenticationError('not logged in')
+
+        }
+    },
+    Mutation:{
+        
+    }
+}
