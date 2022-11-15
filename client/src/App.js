6ee2283bcd11471cc937c7c2477508c559554b1e
night <yseddiq@gmail.com>
2022-11-14 20:33:40 -0500

    added Apollo Provider to App function in App.js to make requests work with Apollo Server

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import{ApolloProvier, ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
+import{ApolloProvider, ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
 import {setContext} from '@apollo/client/link/context';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -7,8 +7,29 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+//sets things up to send a graphql operation to an endpoint over http.
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+//sets things up to be used for authenttication.
+const authLink= setContext((_,{headers})=>{
+  const token=localStorage.getItem('id_token');
+  return{
+    headers:{
+      ...headers,
+      authorization: token? `Bearer ${token}` : '',
+    }
+  };
+});
+//uses the previous two set ups by calling a new instance of ApolloClient.
+const client= new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -19,6 +40,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
